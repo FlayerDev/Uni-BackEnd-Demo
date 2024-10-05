8b4b2ca3aa49f3fc1a2dd34f6939bf33b7d12042
FlayerDev <flayer.dev.gr@gmail.com>
2024-10-05 16:30:45 +0300

    Complementary Commit

---
+++ src/main/kotlin/com/eliaschr/backenddemo/BackenddemoApplication.kt
@@ -0,0 +1,11 @@
+package com.eliaschr.backenddemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackenddemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackenddemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/eliaschr/backenddemo/BackenddemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eliaschr.backenddemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackenddemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
